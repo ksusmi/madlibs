d2c2fcca04e97cffd2a2668431725479f61dd468
ksusmi <susmi.k@gmail.com>
2021-03-17 21:10:01 +0000

    Completed madlibs.py

--- madlibs-solution/madlibs.py
+++ madlibs-solution/madlibs.py
@@ -1,17 +1,16 @@
 """A madlib game that compliments its users."""
 
 from random import choice
-
 from flask import Flask, render_template, request
 
-# "__name__" is a special Python variable for the name of the current module.
+# "__name__" is a special Python variable for the name of the current module
 # Flask wants to know this to know what any imported things are relative to.
 app = Flask(__name__)
 
 AWESOMENESS = [
     'awesome', 'terrific', 'fantastic', 'neato', 'fantabulous', 'wowza',
     'oh-so-not-meh', 'brilliant', 'ducky', 'coolio', 'incredible', 'wonderful',
-    'smashing', 'lovely',
+    'smashing', 'lovely'
 ]
 
 
@@ -31,7 +30,7 @@ def say_hello():
 
 @app.route('/greet')
 def greet_person():
-    """Greet user with compliment."""
+    """Greet user."""
 
     player = request.args.get("person")
 
@@ -41,37 +40,37 @@ def greet_person():
                            person=player,
                            compliment=compliment)
 
+
 @app.route('/game')
 def show_madlib_form():
-    """Ask user would he like to play a game."""
-    answer= request.args.get("game")
+    """Show form to play madlibs or say goodbye."""
 
-    if answer == "yes":
-       return render_template("game.html")
+    play_game = request.args.get("play")
+
+    if play_game == "yes":
+        return render_template("game.html")
     else:
         return render_template("goodbye.html")
 
+
 @app.route('/madlib')
 def show_madlib():
-    """ getting info from game.html """
-    color= request.args.get("color")
-    noun= request.args.get("noun")
-    adjective= request.args.get("adjective")
-    person= request.args.get("person")
-    #madlib_type = choice("funny", "sad")
+    """Show resulting mablib."""
 
-    return render_template("madlib.html", color=color,
-                            noun= noun, person=person, adjective=adjective,
-                            madlib_type = madlib_type
+    name = request.args.get("person")
+    color = request.args.get("color")
+    noun = request.args.get("noun")
+    adjective = request.args.get("adjective")
+
+    return render_template("madlib.html",
+                           person=name,
+                           color=color,
+                           noun=noun,
+                           adjective=adjective,
                            )
-
-
-    
-                           
 
 
 if __name__ == '__main__':
     # Setting debug=True gives us error messages in the browser and also
     # "reloads" our web app if we change the code.
-
     app.run(debug=True, host="0.0.0.0")
